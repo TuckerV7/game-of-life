30975423220b07f9cada3c0bf58742b5f0302cb7
Tucker <191626668+TuckerV7@users.noreply.github.com>
2025-02-16 00:51:49 -0600

    Update 2
    I have completed the game of life!
    ... with only a couple bugs. The top and bottom rows do not work, everything will die there.
    also, when a cell goes off the screen to the right or left it will appear on the other side, except one row down (if its going to the right)
    or one row up (if it's going to the left).
    I also added buttons to show continuous generations and to stop them.
    I will fix these bugs when I can!

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ let makeBoxes = (num) => {
    newCell.id = ('cellid');
    newCell.style.width = '45px';
    newCell.style.height = '45px';
-   newCell.textContent = (`cell${i}`);
    container.appendChild(newCell);
    cellArray.push(`cell${i + 1}`)
    }
@@ -45,9 +44,10 @@ for (let i = 0; i < cellArray.length; i++) {
 // let the cell look at all 8 neighbors
 
 
-//function checkNeighbors(){
+function checkNeighbors(){
 for (let i = 0; i < cellArray.length; i++) {
-      let cellClass = ('.cell'+i);
+      
+   let cellClass = ('.cell'+i);
       let upL =('.cell'+(i-41));
       let up = ('.cell'+(i-40));
       let upR =('.cell'+(i-39));
@@ -66,43 +66,95 @@ for (let i = 0; i < cellArray.length; i++) {
       let downLCell = document.querySelector(downL);
       let leftCell = document.querySelector(left);
       let state = document.querySelector(cellClass);
+      if (!upLCell || !upCell || !upRCell || !rightCell || !downRCell || !downCell || !downLCell || !leftCell){
+
+         continue;
+         };
+   
+
+      let neighborsAlive = (upLCell.classList.contains('alive'))+
+      (upCell.classList.contains('alive'))+
+      (upRCell.classList.contains('alive'))+
+      (rightCell.classList.contains('alive'))+
+      (downRCell.classList.contains('alive'))+
+      (downCell.classList.contains('alive'))+
+      (downLCell.classList.contains('alive'))+
+      (leftCell.classList.contains('alive'));
       
-   
+          if (state.classList.contains('alive') && (neighborsAlive<2)){
+         state.classList.toggle('limbo');
+         
+      } else if (state.classList.contains('alive') && (neighborsAlive == 2 || neighborsAlive == 3)){
+         console.log("I'm still alive!");
+      } else if (state.classList.contains('alive') && (neighborsAlive > 3)){
+         state.classList.toggle('limbo');
+      } else if (state.classList.contains('dead') && (neighborsAlive == 3)){
+         state.classList.toggle('limbo');
+      }
+        
+         
+        
+      } 
+   };
 
+   // after cells have checked neighbors, they determine if they will switch to dead/alive
+   //this function switches their state
+   function changeCells(){
+         for (let i = 0; i < cellArray.length; i++){
+            let cellClass = ('.cell'+i);
+            let state = document.querySelector(cellClass);
 
-   state.addEventListener('click', () => {
-   
-     let neighborsAlive = (upLCell.classList.contains('alive'))+
-     (upCell.classList.contains('alive'))+
-     (upRCell.classList.contains('alive'))+
-     (rightCell.classList.contains('alive'))+
-     (downRCell.classList.contains('alive'))+
-     (downCell.classList.contains('alive'))+
-     (downLCell.classList.contains('alive'))+
-     (leftCell.classList.contains('alive'));
-    if (state.classList.contains('alive') && (neighborsAlive<2)){
-      console.log('I died');
-    } else if (state.classList.contains('alive') && (neighborsAlive == 2 || neighborsAlive == 3)){
-      console.log('Im still alive');
-    } else if (state.classList.contains('alive') && (neighborsAlive > 3)){
-      console.log('i died');
-    } else if (state.classList.contains('dead') && (neighborsAlive == 3)){
-      //It comes to life
-    }
-   
-   
-      });
+            if (state.classList.contains('limbo')){
+               state.classList.toggle('limbo');
+               state.classList.toggle('alive');
+               state.classList.toggle('dead');
+            }
    }
+};
 
-
-/*
+//When this button is pressed it will look at all cells (except the edges) and go through one generation.
 const button = document.querySelector('#btn');
-button.addEventListener('click', checkNeighbors);
+button.addEventListener('click', () => {
+   checkNeighbors();
+   changeCells();
+});
+
+// button to run constant generations, 2 each second
+const continuous = document.querySelector('#multi');
+let intervalid;
+let intervalid2;
+continuous.addEventListener('click', () => {
+ 
+      intervalid = setInterval(checkNeighbors, 500);
+      intervalid2 = setInterval(changeCells, 500);
+
+      
+});
+// button to stop generations
+const stopbtn = document.querySelector('#stop');
+stopbtn.addEventListener('click', () => {
+   setTimeout(() => {
+      clearInterval(intervalid);
+      clearInterval(intervalid2);
+   }, 1);
+});
+   
+
+         
+
+     
+/*
+const multibtn = document.querySelector('#multi');
+multibtn.addEventListener('click', (num) => {
+      for (let i = 0; i < num; i++){
+      checkNeighbors();
+      changeCells();
+   }
+});
 */
 /*
 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overpopulation.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
 */
